7e21e65a812fc6668ed7817f4c2eaae05d51490b
Pablo Musa <pablitomusa@gmail.com>
2019-10-04 20:25:53 +0200

    setup script fixes

--- es_queries/assets/setup_stack.sh
+++ es_queries/assets/setup_stack.sh
@@ -1,5 +1,6 @@
 sudo -H -u packer bash -c '
 cd ~
+unset JAVA_HOME
 
 echo -e "Downloading Elasticsearch..."
 curl -Of --progress-bar https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.4.0-linux-x86_64.tar.gz
@@ -14,9 +15,9 @@ echo -e "Waiting for Elasticsearch to startup..."
 sleep 5;
 while true; do
   #Check to see if its running yet by grepping the cluster_name line
-  local RESPONSE=`curl -s -XGET http://localhost:9200 | grep elasticsearch`
+  RESPONSE=`curl -s -XGET http://localhost:9200 | grep elasticsearch`
   #Get a substring of the response line for simplicity
-  local R2=`echo $RESPONSE | cut -c2-13`
+  R2=`echo $RESPONSE | cut -c2-13`
   if [ "$R2" = "cluster_name" ]; then
     echo -e "Elasticsearch is running!"
     break
@@ -30,12 +31,12 @@ echo -e "Downloading dataset..."
 curl -Of --progress-bar https://gist.githubusercontent.com/pmusa/0413597eba8bed20bd085c579315bbe0/raw/cd944309d2d5974479713deab4ae928cbca317b1/recipes.bulk
 
 echo -e "Loading dataset..."
-curl -XPUT "http://localhost:9200/recipes/_bulk" -H "Content-Type: application/x-ndjson" --data-binary @recipes.bulk
+curl -XPUT "http://localhost:9200/recipes/_bulk" -H "Content-Type: application/x-ndjson" --data-binary @recipes.bulk > /dev/null
 
 echo -e "Downloading Kibana..."
 curl -Of --progress-bar https://artifacts.elastic.co/downloads/kibana/kibana-7.4.0-linux-x86_64.tar.gz
 echo -e "Extracting Kibana..."
 tar -zxf kibana-7.4.0-linux-x86_64.tar.gz
 echo -e "Starting Kibana..."
-./kibana-7.4.0-linux-x86_64/bin/kibana --host=0.0.0.0
+./kibana-7.4.0-linux-x86_64/bin/kibana --host=0.0.0.0 > kibana.log 2>&1 &
 '
