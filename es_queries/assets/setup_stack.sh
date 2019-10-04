c6bc4c8a50906d5d69e139fdc4ac45f47cfce653
Pablo Musa <pablitomusa@gmail.com>
2019-10-04 20:53:44 +0200

    add setup script checks

--- es_queries/assets/setup_stack.sh
+++ es_queries/assets/setup_stack.sh
@@ -12,7 +12,7 @@ echo -e "Starting Elasticsearch..."
 ./elasticsearch-7.4.0/bin/elasticsearch -d
 
 echo -e "Waiting for Elasticsearch to startup..."
-sleep 5;
+sleep 20;
 while true; do
   #Check to see if its running yet by grepping the cluster_name line
   RESPONSE=`curl -s -XGET http://localhost:9200 | grep elasticsearch`
@@ -33,10 +33,36 @@ curl -Of --progress-bar https://gist.githubusercontent.com/pmusa/0413597eba8bed2
 echo -e "Loading dataset..."
 curl -XPUT "http://localhost:9200/recipes/_bulk" -H "Content-Type: application/x-ndjson" --data-binary @recipes.bulk > /dev/null
 
+NUMBER_OF_DOCS=39774
+RESPONSE=`curl -qs "localhost:9200/_cat/count/recipes?h=count"`
+if [ "$RESPONSE" != "$NUMBER_OF_DOCS" ]; then
+  echo -e "There was a Problem ingesting the data. Please, contact support. Exiting..."
+  exit 1 
+fi
+
+echo -e "A total of $NUMBER_OF_DOCS documents were loaded!"
+
 echo -e "Downloading Kibana..."
 curl -Of --progress-bar https://artifacts.elastic.co/downloads/kibana/kibana-7.4.0-linux-x86_64.tar.gz
 echo -e "Extracting Kibana..."
 tar -zxf kibana-7.4.0-linux-x86_64.tar.gz
 echo -e "Starting Kibana..."
 ./kibana-7.4.0-linux-x86_64/bin/kibana --host=0.0.0.0 > kibana.log 2>&1 &
+
+echo -e "Waiting for Kibana to startup..."
+sleep 20;
+while true; do
+  #Check to see if its running yet by grepping the cluster_name line
+  HTTP_CODE=$(curl --write-out %{http_code} --silent --output \
+             /dev/null localhost:5601/api/status)
+  if [ "$HTTP_CODE" = "200" ]; then
+    echo -e "Kibana is running!"
+    break
+  else
+    sleep 3
+    echo -e "Waiting for Kibana to startup..."
+  fi
+done
+
+echo -e "Environment successfully loaded"
 '
